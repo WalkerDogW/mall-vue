30fd2737cc808de68d2434ff5f0bab5f2cb445dd
WalkerDogW <WalkerDogW@outlook.com>
2020-09-02 21:27:49 +0800

    category

--- src/views/modules/product/category.vue
+++ src/views/modules/product/category.vue
@@ -1,25 +1,46 @@
 <template>
-  <el-tree
-    :data="menus"
-    :props="defaultProps"
-    :expand-on-click-node="false"
-    show-checkbox
-    :default-expanded-keys="expandedKey"
-    node-key="catId"
-  >
-    <span class="custom-tree-node" slot-scope="{ node, data }">
-      <span>{{ node.label }}</span>
-      <span>
-        <el-button v-if="node.level<=2" type="text" size="mini" @click="() => append(data)">Append</el-button>
-        <el-button
-          v-if="node.childNodes.length==0"
-          type="text"
-          size="mini"
-          @click="() => remove(node, data)"
-        >Delete</el-button>
+  <div>
+    <el-tree
+      :data="menus"
+      :props="defaultProps"
+      :expand-on-click-node="false"
+      show-checkbox
+      :default-expanded-keys="expandedKey"
+      node-key="catId"
+    >
+      <span class="custom-tree-node" slot-scope="{ node, data }">
+        <span>{{ node.label }}</span>
+        <span>
+          <el-button v-if="node.level<=2" type="text" size="mini" @click="() => append(data)">添加</el-button>
+          <el-button
+            v-if="node.childNodes.length==0"
+            type="text"
+            size="mini"
+            @click="() => remove(node, data)"
+          >删除</el-button>
+          <el-button type="text" size="mini" @click="() => edit(data)">编辑</el-button>
+        </span>
       </span>
-    </span>
-  </el-tree>
+    </el-tree>
+
+    <el-dialog :title="dialogTitle" :visible.sync="dialogVisible" width="30%" :close-on-click-modal="false">
+      <el-form :model="category">
+        <el-form-item label="分类名称">
+          <el-input v-model="category.name" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="图标">
+          <el-input v-model="category.icon" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="计量单位">
+          <el-input v-model="category.productUnit" autocomplete="off"></el-input>
+        </el-form-item>
+      </el-form>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="dialogVisible = false">取 消</el-button>
+        <el-button type="primary" @click="submitData">确 定</el-button>
+      </span>
+    </el-dialog>
+  </div>
 </template>
 
 <script>
@@ -30,7 +51,20 @@ export default {
   data() {
     //这里存数据
     return {
-      expandedKey: [2],
+      category: {
+        catId: null,
+        name: "",
+        parentCid: 0,
+        catLevel: 0,
+        showStatus: 1,
+        sort: 0,
+        icon: null,
+        productUnit: null,
+      },
+      dialogTitle: "",
+      dialogType: "",
+      dialogVisible: false,
+      expandedKey: [],
       menus: [],
       defaultProps: {
         children: "children",
@@ -56,10 +90,50 @@ export default {
         this.menus = data.data;
       });
     },
+    submitData() {
+      if (this.dialogType == "add") {
+        this.addCategory();
+      } else if (this.dialogType == "edit") {
+        this.editCategory();
+      }
+    },
     append(data) {
       console.log("添加数据", data);
+      var emptyCategory = {
+        catId: null,
+        name: "",
+        parentCid: 0,
+        catLevel: 0,
+        showStatus: 1,
+        sort: 0,
+        icon: null,
+        productUnit: null,
+      };
+      this.category = emptyCategory;
+      this.category.parentCid = data.catId;
+      this.category.catLevel = data.catLevel * 1 + 1;
+      this.dialogVisible = true;
+      this.dialogType = "add";
+      this.dialogTitle = "添加分类";
     },
-
+    //添加三级你分类方法
+    addCategory() {
+      console.log("要提交的三级分类数据", this.category);
+      this.$http({
+        url: this.$http.adornUrl("/product/category/save"),
+        method: "post",
+        data: this.$http.adornData(this.category, false),
+      }).then((data) => {
+        this.$message({
+          type: "success",
+          message: "菜单保存成功!",
+        });
+        //刷新出新的菜单
+        this.getMenus();
+        this.expandedKey = [this.category.parentCid];
+        this.dialogVisible = false;
+      });
+    },
     remove(node, data) {
       console.log(node, data);
       this.$confirm(`是否删除【${data.name}】菜单`, "提示", {
@@ -81,7 +155,7 @@ export default {
             //刷新出新的菜单
             this.getMenus();
             //设置需要默认展开的菜单
-            this.expandedKey=[node.parent.data.catId];
+            this.expandedKey = [node.parent.data.catId];
           });
         })
         .catch(() => {
@@ -90,6 +164,41 @@ export default {
             message: "已取消删除",
           });
         });
+    },
+    edit(data) {
+      console.log("要修改的数据", data);
+      this.dialogVisible = true;
+      this.dialogType = "edit";
+      this.dialogTitle = "修改分类";
+      //发送请求获取当前节点最新的数据
+      this.$http({
+        url: this.$http.adornUrl(`/product/category/info/${data.catId}`),
+        method: "get",
+      }).then(({ data }) => {
+        console.log("当前节点最新数据", data);
+        this.category = data.data;
+      });
+      //   this.category.name = data.name;
+      //   this.category.catId = data.catId;
+      //   this.category.icon=data.icon;
+      //   this.category.productUnit=data.productUnit;
+    },
+    //修改三级分类数据
+    editCategory() {
+      this.$http({
+        url: this.$http.adornUrl("/product/category/update"),
+        method: "post",
+        data: this.$http.adornData(this.category, false),
+      }).then((data) => {
+        this.$message({
+          type: "success",
+          message: "菜单修改成功!",
+        });
+        //刷新出新的菜单
+        this.getMenus();
+        this.expandedKey = [this.category.parentCid];
+        this.dialogVisible = false;
+      });
     },
   },
   //声明周期 - 创建完成（可以访问当前this实例）
