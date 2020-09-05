0fac98c1693c7bdc58ad0e34ddb7534013483e53
WalkerDogW <WalkerDogW@outlook.com>
2020-09-05 21:16:56 +0800

    c

--- sql/A_mall_db.sql
+++ sql/A_mall_db.sql
@@ -6,3 +6,4 @@ CREATE DATABASE `mall_nacos` CHARACTER SET 'utf8mb4' COLLATE 'utf8mb4_general_ci
 CREATE DATABASE `mall_order` CHARACTER SET 'utf8mb4' COLLATE 'utf8mb4_general_ci';
 CREATE DATABASE `mall_product` CHARACTER SET 'utf8mb4' COLLATE 'utf8mb4_general_ci';
 CREATE DATABASE `mall_ware` CHARACTER SET 'utf8mb4' COLLATE 'utf8mb4_general_ci';
+CREATE DATABASE `mall_third` CHARACTER SET 'utf8mb4' COLLATE 'utf8mb4_general_ci';
